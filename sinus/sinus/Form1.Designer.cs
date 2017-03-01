816006634c1f4a7b73e2cff68a717f3bcdd01b32
Survial53 <negatorhangkok@gmail.com>
2017-03-01 16:54:18 +0300

    Added sinus regression and multidimensional distribution

---
+++ sinus/sinus/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace sinus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.abs = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ord = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ContextMenuStrip = this.contextMenuStrip1;
+            legend1.DockedToChartArea = "ChartArea1";
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(644, 462);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(225, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(224, 22);
+            this.toolStripMenuItem1.Text = "toolStripMenuIteыврыврm1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(769, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Пщ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(665, 359);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.abs,
+            this.ord});
+            this.dataGridView1.Location = new System.Drawing.Point(662, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(182, 150);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // abs
+            // 
+            this.abs.HeaderText = "X";
+            this.abs.Name = "abs";
+            this.abs.Width = 60;
+            // 
+            // ord
+            // 
+            this.ord.HeaderText = "Y";
+            this.ord.Name = "ord";
+            this.ord.Width = 60;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(769, 183);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Generate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(663, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Points source:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 26);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(75, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Location = new System.Drawing.Point(662, 212);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(97, 127);
+            this.panel1.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Max";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Min";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(662, 183);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 559);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn abs;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ord;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ sinus/sinus/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinus
+{
+    public partial class Form1 : Form
+    {
+        private double[] X;
+        private double[] Y;
+        private double[] P = { 0, 1, 1, 0 };
+        private double[] P_prev = { 0, 0, 0, 0 };
+        private string path = @"./points.txt";
+        private double eps = 0.0001;
+        public delegate double funcD(double x, double y, double[] p);
+                
+        public void readDAta(string s)
+        {
+            string[] data;
+            List<double> xv = new List<double>();
+            List<double> yv = new List<double>();
+            data = File.ReadAllLines(s);
+
+            int src = 1;
+
+            int i = 0;
+            while (i < data.Length)
+            {
+                if (data[i].CompareTo("") == 0)
+                {
+                    src = 2;
+                    i++;
+                }
+                
+                switch (src)
+                { 
+                    case 1:
+                        xv.Add((Convert.ToDouble(data[i].Replace('.', ','))) / 10);
+                        break;
+                    case 2:
+                        yv.Add((Convert.ToDouble(data[i].Replace('.', ','))) / 10);
+                        break;
+                }
+                i++;
+            }
+            
+            Y = yv.ToArray();
+
+            /*double t = 0;
+            for (i = 0; i < Y.Length; i++)
+            {
+                xv.Add(t);
+                t += 0.1;
+            }*/
+            X = xv.ToArray();
+
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "0,01";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Enabled = false;
+            textBox3.Text = (10).ToString();
+            textBox2.Text = (-10).ToString();
+        }
+
+        public double func(double x, double[] p)
+        {
+            return p[0] + p[1] * Math.Sin(p[2] * x + p[3]);
+        }
+
+        /* dS/dA */
+        public double func_da(double x, double y, double[] p)
+        {
+            return 2 * (p[0] + p[1] * Math.Sin(p[2] * x + p[3]) - y);
+        }
+
+        /* dS/dB */
+        public double func_db(double x, double y, double[] p)
+        {
+            return 2 * Math.Sin(p[2] * x + p[3]) * (p[0] + p[1] * Math.Sin(p[2] * x + p[3]) - y);
+        }
+
+        /* dS/dC */
+        public double func_dc(double x, double y, double[] p)
+        {
+            return 2 * p[1] * x * Math.Cos(p[2] * x + p[3]) * (p[0] + p[1] * Math.Sin(p[2] * x + p[3]) - y);
+        }
+
+        /* dS/dD */
+        public double func_dd(double x, double y, double[] p)
+        {
+            return 2 * p[1] * Math.Cos(p[2] * x + p[3]) * (p[0] + p[1] * Math.Sin(p[2] * x + p[3]) - y);
+        }
+
+        /* S */
+        public double func_S(double x, double y, double[] p)
+        {
+            //return y * y - 2 * y * p[0] - 2 * y * p[1] * Math.Sin(p[2] * x + p[3]) + p[0] * p[0] + 2 * p[0] * p[1] * Math.Sin(p[2] * x + p[3]) + p[1] * p[1] * Math.Pow(Math.Sin(p[2] * x + p[3]), 2) ;
+            return Math.Pow(y - p[0] - p[1] * Math.Sin(p[2] * x + p[3]), 2);
+        }
+
+        public double Sum(double[] p, funcD f)
+        {
+            double sum = 0;
+            for (int i = 0; i < X.Length; i++)
+            {
+                sum += f(X[i], Y[i], p);
+            }
+            return sum;
+        }
+
+        public double[] CalculateGrad()
+        {
+            double[] t = new double[4];
+         
+            t[0] = Sum(P, func_da);
+            t[1] = Sum(P, func_db);
+            t[2] = Sum(P, func_dc);
+            t[3] = Sum(P, func_dd);
+            return t;
+        }
+
+        public double CalculateLambda(double[] grad)
+        {
+            double lamb = double.Epsilon, lamb_prev = double.Epsilon;
+            double dsum = 0, ddsum = 0;
+            double[] pr = new double[4];
+            double[] dpr = new double[4];
+            double sin, cos;
+
+            do
+            {
+                dsum = 0;
+                ddsum = 0;
+                lamb_prev = lamb;
+
+                for (int i = 0; i < pr.Length; i++)
+                {
+                    pr[i] = P[i] - lamb * grad[i];
+                    dpr[i] = -grad[i];
+                }
+
+                for (int i = 0; i < X.Length; i++)
+                {
+                    sin = Math.Sin(pr[2] * X[i] + pr[3]);
+                    cos = Math.Cos(pr[2] * X[i] + pr[3]);
+                    dsum += 2 * (-Y[i] + pr[0] + pr[1] * sin) * (dpr[0] + dpr[1] * sin + pr[1] * cos * (dpr[2] * X[i] + dpr[3]));
+                }
+
+                for (int i = 0; i < X.Length; i++)
+                {
+                    sin = Math.Sin(pr[2] * X[i] + pr[3]);
+                    cos = Math.Cos(pr[2] * X[i] + pr[3]);
+                    ddsum += 2 * (Math.Pow(dpr[0] + dpr[1] * sin + pr[1] * cos * (dpr[2] * X[i] + dpr[3]), 2) + (-Y[i] + pr[0] + pr[1] * sin) * (2 * dpr[1] * (dpr[2] * X[i] + dpr[3]) * cos - Math.Pow(dpr[2] * X[i] + dpr[3], 2) * pr[1] * sin));
+                }
+
+                lamb = lamb_prev - dsum / ddsum;
+            }
+            while (lamb - lamb_prev > 0.001);
+            return lamb;
+        }
+
+        public void Calculate()
+        {
+            button1.Enabled = false;
+            double[] gradient;
+            double lambda = 0;
+            double sum1, sum2;
+            double comp = Convert.ToDouble(textBox1.Text);
+
+            int mxi = Array.IndexOf(Y, Y.Max());
+            int mni = Array.IndexOf(Y, Y.Min());
+
+            P[0] = (Y.Max() + Y.Min()) / 2.0;
+            P[1] = (Y.Max() - Y.Min()) / 2.0;
+            P[2] = (Math.Asin((Y.Max() - P[0]) / P[1]) - (Math.Asin((Y.Min() - P[0]) / P[1]))) / (X[mxi] - X[mni]);
+            P[3] = Math.Asin((Y.Min() - P[0]) / P[1]) - P[2] * X[mni];
+
+            do
+            {
+                P.CopyTo(P_prev, 0);
+                gradient = CalculateGrad();
+                lambda = CalculateLambda(gradient);
+                for (int i = 0; i < P.Length; i++)
+                {
+                    P[i] -= lambda * gradient[i];
+                }
+                sum1 = Sum(P_prev, func_S);
+                sum2 = Sum(P, func_S);
+            } while (sum1 - sum2 > comp);
+            button1.Enabled = true;
+        }
+        
+        public void chartBuild(double[] x, double[] y, int series)
+        {
+            chart1.Series[series].Points.Clear();
+            chart1.Series[series].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastPoint;
+            
+            for (int j = 0; j < x.Count(); j++)
+            {
+                chart1.Series[series].Points.AddXY(x[j], y[j]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double mx = X.Max();
+            double mi = X.Min();
+            chartBuild(X, Y, 0);
+            Calculate();
+            chart1.Series[1].Points.Clear();
+            for (double v = mi, step = (mx - mi) / 10000; v < mx; v += step)
+            {
+                chart1.Series[1].Points.AddXY(v, func(v, P));
+            }
+            
+        }
+
+        public void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        private void generatePoints()
+        { 
+            double max = 0, min = 0;
+            max = Convert.ToDouble(textBox3.Text);
+            min = Convert.ToDouble(textBox2.Text);
+            if (max < min)
+            {
+                Swap(ref max, ref min);
+            }
+
+           /* for (double v = min, step = (max - min) / 100; v < max; v += step)
+            {
+                
+            }*/
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            readDAta(path);
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < X.Length; i++)
+            {
+                dataGridView1.Rows.Add(X[i], Y[i]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            generatePoints();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (X != null && Y != null)
+            {
+                int col = e.ColumnIndex;
+                switch (col)
+                {
+                    case 0:
+                        {
+                            X[e.RowIndex] = Convert.ToDouble(dataGridView1.Rows[e.RowIndex].Cells[col].Value);
+                            break;
+                        }
+                    case 1:
+                        {
+                            Y[e.RowIndex] = Convert.ToDouble(dataGridView1.Rows[e.RowIndex].Cells[col].Value);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+           
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox1.Checked)
+            {
+                panel1.Enabled = false;
+                button2.Enabled = true;
+            }
+            else
+            {
+                panel1.Enabled = true;
+                button2.Enabled = false; 
+            }
+
+            
+        }
+    }
+}
